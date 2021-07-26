d713ac89a6701c01409ee623bfb215ff44f6a1a0
whalewalker <mongodbms@gmail.com>
2021-07-26 22:41:42 +0100

    added readme file to project

---
+++ src/main/java/com/example/recruiterbackend/RecruiterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.recruiterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruiterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruiterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recruiterbackend/RecruiterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recruiterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruiterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
